74c2649fcbdd21f1a2fbebff0a33e7746fbaf228
Rodolpho Pinheiro <rhbpinheiro30@gmail.com>
2023-10-09 20:11:18 -0300

    base

---
+++ src/controllers/pageComtroller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+    res.send('no controller')
+
+}
+export const dogs = (req: Request, res: Response) => {
+
+}
+export const cats = (req: Request, res: Response) => {
+
+}
+export const fishes = (req: Request, res: Response) => {
+
+}
